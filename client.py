6854b095770157882fe9c5da22aeff9d7887c2c0
boar401s2 <johnsBogAcc@gmail.com>
2014-05-20 18:56:18 +1000

    Added tcp_serial_bridge.py

--- client.py
+++ client.py
@@ -2,11 +2,17 @@ import pygame, socket
 
 HOST = ("192.168.52.3", 80)
 
+MAX_SPEED = 400
+PULSE_MIDDLE = 1500
+
 class Application:
     def __init__(self):
         self.running = False
         self.surface = None
         self.joystick = None
+        
+        self.joy_pos = (0, 0)
+        self.speeds = (0, 0)
 
     def start(self):
         pygame.init()
@@ -18,8 +24,8 @@ class Application:
         print "Joystick:", self.joystick.get_name()
         self.joystick.init()
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(HOST)
+        #self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #s.connect(HOST)
         
         self.running = True
         self.loop()
@@ -29,9 +35,29 @@ class Application:
 
     def handleEvents(self):
         for event in pygame.event.get():
-            print event
             if event.type == pygame.QUIT:
-                self.stop()                
+                self.stop()
+            elif event.type == pygame.JOYAXISMOTION:
+                if event.value > 1 or event.value < -1:
+                    continue
+
+                if event.axis == 1:
+                    self.joy_pos = self.joy_pos[0], -float("{0:.2f}".format(event.value))
+                elif event.axis == 0:
+                    self.joy_pos = float("{0:.2f}".format(event.value)), self.joy_pos[1]
+
+                self.calculateSpeeds()
+
+    def calculateSpeeds(self):
+        left = MAX_SPEED*self.joy_pos[1]
+        right = left
+
+        if self.joy_pos[0] < 0:
+            left = left - (right*abs(self.joy_pos[0]))*2
+        else:
+            right = right - (left*abs(self.joy_pos[0]))*2
+        
+        self.speeds = left, right
 
     def loop(self):
         #While the running flag is true...
--- server.py
+++ server.py
@@ -1,1 +1,75 @@
 import socket, serial
+
+PORT = ""
+
+class Application:
+    def __init__(self):
+        self.running = False
+
+        self.buffer = ""
+        self.hasPacketStarted = None
+        self.commands = ["SHUTDOWN", "HELP"]
+
+        self.conn = None
+
+    def start(self):
+        self.running = True
+
+        self.serial = serial.Serial(PORT)
+        print "Connected to serial port:", self.serial.name
+        ser.write(
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(("0.0.0.0", 1997))
+        self.socket.listen(1)
+        print "Running server on:", ("0.0.0.0", 1997)
+
+        self.loop()
+
+    def stop(self):
+        self.running = False
+
+    def processByte(self, byte):
+        if byte == "[" and not self.hasPacketStarted:
+            self.hasPacketStarted = True
+        elif byte == "]" and self.hasPacketStarted:
+            self.hasPacketStarted = False
+            self.onPacketRecieved(self.buffer)
+            self.buffer = ""
+        elif self.hasPacketStarted:
+            self.buffer = self.buffer + byte
+
+    def onPacketRecieved(self, packet):
+        print "Packet Recieved:", packet
+        
+        cmd = packet.split(" ")[0]
+        params = packet.split(" ")
+        del params[0]
+        
+        if not cmd in self.commands:
+            self.conn.send("NACK\n")
+            return
+
+        if cmd == "SHUTDOWN":
+            self.stop()
+            self.conn.send("ACK\n")
+        elif cmd == "HELP":
+            self.conn.send("COMMANDS:\n")
+            for command in self.commands:
+                self.conn.send(" - "+command+"\n")
+            self.conn.send("ACK\n")
+
+    def loop(self):
+        self.conn, addr = self.socket.accept()
+        self.conn.send("ACK\n")
+        print "Connection from: ", addr
+        buff = []
+        while self.running:
+            self.processByte(self.conn.recv(1))
+            
+        self.conn.close()
+        quit()
+
+if __name__ == "__main__":
+    app = Application()
+    app.start()
